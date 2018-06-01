76d44570fc3f60a0285d4faab53e33eb44bf048f
xin <xin.sun36@gmail.com>
2018-06-01 11:24:21 +0200

    feat(go-example/copy_slice):print pointer

--- copy_slice/main.go
+++ copy_slice/main.go
@@ -7,14 +7,14 @@ func main() {
 	l1 := []int{1, 2, 3, 4, 5}
 	l2 := l1
 	l2[0] = 99
-	fmt.Println("list 1:", l1)
-	fmt.Println("list 2:", l2)
+	fmt.Printf("list 1: %p %v \n", &l1[0], l1)
+	fmt.Printf("list 2: %p %v \n", &l2[0], l2)
 	fmt.Println()
 
 	fmt.Println("copy list by copy()")
 	l3 := make([]int, len(l1))
 	copy(l3, l1)
 	l3[0] = -1
-	fmt.Println("list 1:", l1)
-	fmt.Println("list 3:", l3)
+	fmt.Printf("list 1: %p %v \n", &l1[0], l1)
+	fmt.Printf("list 3: %p %v \n", &l3[0], l3)
 }
